75133da0d85b14b72dedfefa7616bc24439f3ed1
Martina Brocchi <73938003+marti-brocchi@users.noreply.github.com>
2021-11-13 19:12:43 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <title>Log-out</title>
+</head>
+
+<body>
+
 <?php
-  session_start(); // recupera la sessione esistente
+    session_start(); // recupera la sessione esistente
 
-  /* Chiudo la sessione e rimando alla homepage */
-  $_SESSION = array();
-  session_destroy(); 
+    /* Chiudo la sessione e rimando alla homepage */
+    $_SESSION = array();
+    setcookie(session_name(),'',time() - 42000); //Cancello cookie di sessione
+    session_destroy(); 
 
-  header("Location: homepage.php");
+    header("Location: homepage.php");
 ?>
+
+</body>
+</html>
