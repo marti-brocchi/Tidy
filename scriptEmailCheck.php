eb465e2fa143df064300e0841e229e1d2023c69c
LorenzoLaCorte <49609738+LorenzoLaCorte@users.noreply.github.com>
2021-12-08 18:39:25 +0100

    Add files via upload

---
+++ scriptEmailCheck.php
@@ -0,0 +1,21 @@
+<?php
+	include("Comuni/DB_connect.php");
+
+  /* SE LA MAIL ESISTE GIA', LO SCRIPT TORNA FALSE */
+  $email = $_GET["email"];
+
+  $stmt = mysqli_prepare($con, "SELECT firstname, lastname, email, pass FROM utenti WHERE email=?");
+  mysqli_stmt_bind_param($stmt, 's', $email);
+  mysqli_stmt_execute($stmt);
+
+  $res = mysqli_stmt_get_result($stmt);
+  $row = mysqli_fetch_assoc($res); 
+  mysqli_stmt_close($stmt);
+
+  // Se la mail esiste già il risultato sarà composto da una (o piu') righe
+  // e ritorno false; se va tutto bene lo script torna true
+  if(mysqli_num_rows($res) != 0)
+    echo "false";
+  else
+    echo "true";
+?>
