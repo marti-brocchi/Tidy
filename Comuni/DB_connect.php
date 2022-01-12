929aabe222d0d352985a5e127719012f60f13a23
LorenzoLaCorte <49609738+LorenzoLaCorte@users.noreply.github.com>
2022-01-12 15:08:13 +0100

    Delete Comuni directory

--- Comuni/DB_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $con = mysqli_connect('localhost', '4784539', 'LaxMarti2122!', 'saw_progetto_21_db');
-
-  if (mysqli_connect_errno($con)) {
-    echo "<h1> Qualcosa è andato storto :( </h1>";
-    exit();
-  } 
-?>
